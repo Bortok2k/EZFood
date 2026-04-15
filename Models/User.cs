79ea42aae514948904adc58631aaba18f1f60383
Bortok2k <luismiparra2000@gmail.com>
2026-04-15 16:33:21 -0500

    Menu Principal V1

---
+++ Models/User.cs
@@ -0,0 +1,8 @@
+﻿namespace EZFood.Models
+{
+    public class User
+    {
+        public int rol {  get; set; }
+        public string user_name { get; set; }
+    }
+}
---
+++ Services/UserService.cs
@@ -0,0 +1,25 @@
+﻿using EZFood.Models;
+using System.Net.Http.Json;
+
+namespace EZFood.Services
+{
+    public class UserService
+    {
+        private readonly HttpClient _http;
+
+        public UserService(HttpClient http)
+        {
+            _http = http;
+        }
+
+        public async Task<List<User>> GetUsers()
+        {
+            return await _http.GetFromJsonAsync<List<User>>("users");
+        }
+
+        public async Task CreateUser(User user)
+        {
+            await _http.PostAsJsonAsync("users", user);
+        }
+    }
+}
