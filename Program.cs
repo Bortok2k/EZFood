f3a4c723efbafeceb39d31cc91548ecbb8c51e79
Bortok2k <luismiparra2000@gmail.com>
2026-04-18 22:24:24 -0500

    Modulo Inventario

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:8080/api/v1") });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:8080") });
 builder.Services.AddScoped<UserService>();
 
 await builder.Build().RunAsync();
