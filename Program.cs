81a390dc773023a423486d6a578e0229de83b8c9
Bortok2k <luismiparra2000@gmail.com>
2026-05-12 20:33:18 -0500

    Implementacion de EZBot y Organizacion de MainLayout Centralizado

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 using EZFood;
-using EZFood.Services;
+using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -8,6 +8,24 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:8080") });
-builder.Services.AddScoped<UserService>();
+builder.Services.AddScoped<SidebarService>();
 
 await builder.Build().RunAsync();
+
+public class SidebarService
+{
+    public RenderFragment SidebarButtons { get; private set; }
+    public event Action OnChange;
+
+    public void SetButtons(RenderFragment buttons)
+    {
+        SidebarButtons = buttons;
+        OnChange?.Invoke();
+    }
+
+    public void Clear()
+    {
+        SidebarButtons = null;
+        OnChange?.Invoke();
+    }
+}
