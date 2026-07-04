c334fdc5759c0c4be1eb94a060bf3af22837115b
Bortok2k <luismiparra2000@gmail.com>
2026-07-04 01:18:06 -0500

    Implementacion de Login y creacion de usuarios

---
+++ AuthorizedHttpClientHandler.cs
@@ -0,0 +1,17 @@
+﻿namespace EZFood
+{
+    public class AuthorizedHttpMessageHandler : DelegatingHandler
+    {
+        protected override async Task<HttpResponseMessage> SendAsync(
+            HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            if (!string.IsNullOrEmpty(AuthState.Token))
+            {
+                request.Headers.Authorization =
+                    new System.Net.Http.Headers.AuthenticationHeaderValue(
+                        "Bearer", AuthState.Token);
+            }
+            return await base.SendAsync(request, cancellationToken);
+        }
+    }
+}
